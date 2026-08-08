0a530f33d80ba0d358732301cfb42c2e9646f084
Cedric Hutchings <cedric@passivelogic.com>
2026-08-07 20:28:07 -0600

    switch to SDL

--- README.md
+++
@@ -1,3 +0,0 @@
-`cmake -DCMAKE_BUILD_TYPE=Debug -S . -B build && cd build && make && ./Debug/spat`
-
-`cmake -DCMAKE_BUILD_TYPE=Release -DSDL_STATIC=ON -DSDL_SHARED=OFF -S . -B build && cd build && make && ./Release/spat`
--- main.c
+++ main.c
@@ -1,101 +1,55 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <math.h>
-#include <MiniFB.h>
+#define SDL_MAIN_USE_CALLBACKS 1
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
 
-#define dbg() __builtin_debugtrap()
-#define spat_min(x, y) (((x) < (y)) ? (x) : (y))
-#define spat_max(x, y) (((x) > (y)) ? (x) : (y))
+static SDL_Window *window = NULL;
 
-#include "font.h"
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
+    SDL_SetAppMetadata("Space Port Arms Technician", "1.0", "com.problemchild.spat");
 
-static struct {
-    uint32_t  offset_x, offset_y, size_x, size_y, canvas_scale;
-    uint32_t *buf;
-} spat = {
-    .size_x = 160,
-    .size_y =  90,
-};
-
-static void spat_resize(struct mfb_window *window, int size_x, int size_y) {
-    float scale = floorf(fmin(
-        (float)size_x / spat.size_x,
-        (float)size_y / spat.size_y
-    ));
-    spat.canvas_scale = scale;
-    if (scale <= 0.0f) {
-        mfb_set_viewport(window, 0, 0, 1, 1);
-        return;
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
     }
 
-    float sx = scale * spat.size_x;
-    float sy = scale * spat.size_y;
-    float x = (size_x - sx) * 0.5;
-    float y = (size_y - sy) * 0.5;
-    spat.offset_x = x;
-    spat.offset_y = y;
-    mfb_set_viewport(window, x, y, sx, sy);
+    window = SDL_CreateWindow(
+        "spat", 640, 480,
+        SDL_WINDOW_RESIZABLE
+    );
+    if (window == NULL) {
+        SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    return SDL_APP_CONTINUE;
 }
 
-int main() {
-    mfb_set_log_level(0);
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
+    if (event->type == SDL_EVENT_QUIT) {
+        return SDL_APP_SUCCESS;
+    }
+    return SDL_APP_CONTINUE;
+}
 
-    spat.canvas_scale = 5;
-    struct mfb_window *window = mfb_open_ex(
-        "spat",
-        spat.size_x*spat.canvas_scale,
-        spat.size_y*spat.canvas_scale,
-        MFB_WF_RESIZABLE | MFB_WF_BORDERLESS
-    );
-    if (!window) return -1;
-    spat_resize(window, spat.size_x*spat.canvas_scale, spat.size_y*spat.canvas_scale);
+SDL_AppResult SDL_AppIterate(void *appstate) {
+    SDL_Surface *surface = SDL_GetWindowSurface(window);
 
-    spat.buf = (uint32_t *) malloc(spat.size_x * spat.size_y * 4);
-    mfb_set_resize_callback(window, spat_resize);
+    Uint32 *pixels = (Uint32 *)surface->pixels;
+    int pitch = surface->pitch / sizeof(Uint32);
 
-    mfb_update_state state;
-    do {
-        for (uint32_t i = 0; i < spat.size_x * spat.size_y; ++i) {
-            uint32_t x = i % spat.size_x;
-            uint32_t y = i / spat.size_x;
+    for (int y = 0; y < surface->h; ++y) {
+        for (int x = 0; x < surface->w; ++x) {
+            Uint8 r = (Uint8)(x * 255 / surface->w);
+            Uint8 g = (Uint8)(y * 255 / surface->h);
+            Uint8 b = 128;
 
-            spat.buf[i] = (x ^ y)%2 ? MFB_ARGB(0x44, 0x44, 0x44, 0x44) : MFB_ARGB(0x22, 0x22, 0x22, 0x22);
+            pixels[y * pitch + x] =
+                SDL_MapSurfaceRGB(surface, r, g, b);
         }
+    }
 
-        {
-            int mouse_x = mfb_get_mouse_x(window) - (int)spat.offset_x;
-            int mouse_y = mfb_get_mouse_y(window) - (int)spat.offset_y;
-            uint32_t wx = spat_max(0, spat_min((int)spat.size_x-1, mouse_x / (int)spat.canvas_scale));
-            uint32_t wy = spat_max(0, spat_min((int)spat.size_y-1, mouse_y / (int)spat.canvas_scale));
-            spat.buf[wy*spat.size_x + wx] = MFB_ARGB(0xff, 0xff, 0xff, 0xff);
-        }
+    SDL_UpdateWindowSurface(window);
+}
 
-        {
-            uint32_t dst_x = 15;
-            uint32_t dst_y = 15;
-
-            for (char *str = "The five boxing wizards jump quickly"; *str; str++) {
-                for (int y = 0; y < 8; y++) {
-                    char row = font_data[((size_t)*str)*8 + y];
-                    for (int x = 0; x < 8; x++) {
-                        bool lit = (row & (1 << (8 - x))) > 0;
-
-                        if (!lit) continue;
-                        spat.buf[(dst_y + y)*spat.size_x + (dst_x + x)] = MFB_ARGB(0xff, 0xff, 0xff, 0xff);
-                    }
-                }
-
-                dst_x += 6;
-            }
-        }
-
-        state = mfb_update_ex(window, spat.buf, spat.size_x, spat.size_y);
-        if (state != MFB_STATE_OK) {
-            window = NULL;
-            break;
-        }
-    } while (mfb_wait_sync(window));
-
-    return 0;
+void SDL_AppQuit(void *appstate, SDL_AppResult result) {
 }
